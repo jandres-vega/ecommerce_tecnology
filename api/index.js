35ac36aae91d37478f7a6cd08b1968c071832541
Andres vega <jandresvega@itsa.edu.co>
2022-07-16 09:19:23 -0500

    modificando archivo index

--- api/index.js
+++ api/index.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./src/libs/conexion');
+const routes = require('./src/routes/index')
+const {logError, errorHandler, boomErrorHandler} = require('./src/middlewares/error.handles')
 const app = express();
 const port = process.env.PORT;
-
-const {logError, errorHandler, boomErrorHandler} = require('./src/middlewares/error.handles')
-//los middlewares de tipo error seimpre se deben colocar despues
-//de routing y tambie ahi que ver en que orden se va ejecutar uno tras del otro
-//y es importtante xq con ayuda del next es quien va seguir una tras del otro
-
 
 // para hacer peticiones de tipo post es necesario
 //usar este middleware
 app.use(express.json());
 
+
+routes(app);
+
+
+//los middlewares de tipo error seimpre se deben colocar despues
+//de routing y tambie ahi que ver en que orden se va ejecutar uno tras del otro
+//y es importtante xq con ayuda del next es quien va seguir una tras del otro
 app.use(logError);
 app.use(boomErrorHandler)
 app.use(errorHandler)
